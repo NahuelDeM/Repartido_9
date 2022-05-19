65bb6b2c509e583a0436fe767849b9f00477308e
NahuelDeM <Bios.Nahuel@gmail.com>
2022-05-19 18:04:25 -0300

    Modulo2

--- Script.js
+++ Script.js
@@ -1,8 +1,31 @@
-function traerEstudiantes(){
-    fetch("Prueba.json")
-    .then(Prueba => Prueba.json())
-    .then(listaEst => {
-        console.log("dataEst", listaEst);
+function traercasa(){
+    fetch("Datos.json")
+    .then(Datos => Datos.json())
+    .then(traerCasa => {
+        objetos(traerCasa)
     })
 }
-traerEstudiantes();
+traercasa();
+
+var traelascasas = []
+var todojunto = ``
+function objetos(traelascasas){
+    for (i = 0; i < traelascasas.length; i++ ){
+        jose = traelascasas[i]
+        todojunto += `
+        <div>
+            <img src="${jose.img}" alt="Property 1">
+            <div class="Property-details">
+                <p class="price">${jose.precio}</p>
+                <span class="beds">${jose.Habitaciones} |</span>
+                <span class="baths">${jose.Baños} |</span>
+                <span class="sqft">${jose.Metroscuadrados}</span>
+                <address>
+                    ${jose.Dirección}
+                </address>
+            </div>
+        </div>`
+    }
+    
+    document.getElementById("properties-slider").innerHTML = todojunto
+}
